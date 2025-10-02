88e8d4b3535aaea02cf6f58365ca6fef5f2ab04b
coluccifrancesco <coluccif2002@gmail.com>
2025-10-02 02:56:45 +0200

    First style and skeleton idea

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>Best New Passwords</title>
 </head>
-<body>
-    
+
+<body class="text-white bg-dark">
+    <header class="mt-5">
+        <h1 class="text-center">Have you ever felt really safe?</h1>
+        <h3 class="text-center">If no, you should try our passwords...</h3>
+        <p class="text-center">Try them anyway</p>
+    </header>
+
+    <main>
+        <section class="container-fluid">
+            <div class="mx-5 my-5 row">
+                <div class="border rounded bg-primary my-2 col-12 col-md-6">
+                    g
+                </div>
+                <div class="d-none d-md-block col-1"></div>
+                <div class="border rounded bg-primary my-2 col-12 col-md-5">
+                    g
+                </div>
+            </div>
+        </section>
+    </main>    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
